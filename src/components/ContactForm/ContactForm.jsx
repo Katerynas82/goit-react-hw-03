3f56ad14f43404ba4848f7d9da9c12c95b0c04cf
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-09-12 23:25:11 +0100

    3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,75 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const ContactForm = ()=> {
-return (
-    <form>
-        <input></input>
-        <input></input>
-        <button>Add Contact</button>
-    </form>
-);
+import styles from "../ContactForm/ContactForm.module.css";
+
+
+const phoneRegExp = /^(\+380|0)\d{9}$/;
+
+const ContactFormSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too Short! Min 3 simbols.")
+    .max(50, "Too Long!")
+    .required("Required"),
+  phoneNum: Yup.string()
+    .matches(
+      phoneRegExp,
+      "Invalid phone number. Must be a valid Ukrainian number")
+    .required("Phone number is required"),
+});
+
+const ContactForm = () => {
+  const initialValues = {
+    username: "",
+    phoneNum: "",
+  };
+
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={ContactFormSchema}
+      >
+        <Form className={styles.form}>
+          <label className={styles.label}>
+            <span>Name</span>
+            <Field
+              name="username"
+              className={styles.input}
+              placeholder="Add contact name"
+            />
+            <ErrorMessage
+              name="username"
+              component='p'
+              className={styles.error}
+            />
+          </label>
+
+          <label className={styles.label}>
+            <span>Phone number</span>
+            <Field
+              type="tel"
+              name="phoneNum"
+              className={styles.input}
+              placeholder="Add contact phone"
+            />
+            <ErrorMessage
+              name="phoneNum"
+              component='p'
+              className={styles.error}
+             />
+          </label>
+          <button type="submit">Add Contact</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
-export  default ContactForm;
+export default ContactForm;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import styles from "../ContactForm/ContactForm.module.css"
 
 const SearchBox =()=>{
-    return(
-<>
-<input></input>
-</>
+     const [inputValue, setInputValue] = useState("");
 
+     const handleChange = (evt) => {
+       setInputValue(evt.target.value);
+     };
+    return (
+      <div className={styles.form}>
+        <label className={styles.label}>
+          <span>Find contacts by name</span>
+          <input type="text" className={styles.input} value={inputValue} onChange={handleChange} />
+        </label>
+      </div>
     );
 };
  export default SearchBox;
